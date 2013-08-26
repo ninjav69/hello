d1f233694eb396fee87a2c4960e7d7f4638b8889
ninjav69 <al.ninjav@gmail.com>
2013-08-26 10:41:13 +0200

    second commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: apickard
+ *
+ * Created on August 26, 2013, 10:33 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
